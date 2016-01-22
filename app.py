226bebcca836bd4ff06ba4b37c7d17d6b2d76e72
Abhinay Omkar <abhiomkar@google.com>
2016-01-22 17:36:11 +0530

    added static files - css and templates. Welcome home!

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def home():
+    return render_template("home.html", name="home")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
